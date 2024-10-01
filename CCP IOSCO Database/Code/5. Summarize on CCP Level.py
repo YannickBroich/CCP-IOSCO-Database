157b412678e48ab4f149fdaf5d985ec27d3035ba
YannickBroich <161732627+YannickBroich@users.noreply.github.com>
2024-10-01 22:15:23 +0200

    Update 5. Summarize on CCP Level.py

--- CCP IOSCO Database/Code/5. Summarize on CCP Level.py
+++ CCP IOSCO Database/Code/5. Summarize on CCP Level.py
@@ -8,7 +8,7 @@ Created on Tue Oct  1 13:43:03 2024
 import pandas as pd
 
 
-file_path = r"C:\{Your Path}\CCP IOSCO Database - Arbeitsversion\Database\CCP_IOSCO_Database2.xlsx"
+file_path = r"C:\{Your Path}\CCP IOSCO Database\Database\CCP_IOSCO_Database2.xlsx"
 
 
 df = pd.read_excel(file_path)
@@ -24,7 +24,7 @@ numeric_columns = df.select_dtypes(include=['number']).columns.tolist()
 summed_df = df.groupby(['CCP', 'ReportDate'])[numeric_columns].sum().reset_index()
 
 
-output_file_path = r"C:\{Your Path}\CCP_IOSCO_Database_Summed.xlsx"
+output_file_path = r"C:\{Your Path}\CCP IOSCO Database\Database\CCP_IOSCO_Database_Summed.xlsx"
 summed_df.to_excel(output_file_path, index=False)
 
 print("Summierung abgeschlossen und Datei gespeichert unter:", output_file_path)
